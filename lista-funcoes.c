d40b6901453032056e84449ba0a66ce40b3ef312
Joelma Regina <84204532+joelmaregina@users.noreply.github.com>
2022-08-26 20:22:56 -0300

    Update lista-funcoes.c

--- lista-funcoes.c
+++ lista-funcoes.c
@@ -20,6 +20,42 @@ int soma(int a, int b)
     return a + b;
 }
 
+//  03. Faça um programa que tenha uma função que recebe um número inteiro e retorna o fatorial deste número. 
+//  Esta função deve ainda verificar se é possível calcular o fatorial, se não for, ela deve de alguma forma retornar
+//  para a main que deu algum erro. A função main deve solicitar o valor do usuário e imprimir o fatorial dele, 
+//  ou se não é possível calcular o fatorial.
+#include <stdio.h>
+
+
+int fatorial(int valor)
+{
+    int fatorial = 1;
+    for(int i = 1; i <= valor; i++){
+  
+        fatorial = fatorial * i;
+    }
+    return fatorial;
+}
+
+int main()
+{
+    int i;
+    int valor;
+    int resultado;
+    
+    printf("Digite o número de entrada: \n");
+    scanf("%d", &valor);
+    
+    if (valor < 0){
+        printf("Não é possível fazer fatorial de um número negativo");
+    }else{
+        resultado = fatorial(valor);
+        
+        printf("%d", resultado);
+        
+    } 
+}
+
 //  02. Crie um programa que tenha uma função subtrai e a função main.
 //  A função main deve ler três valores, enviar para a função subtrai.
 //  A função subtrai deve realizar a subtração dos três valores (primeiro menos o segundo menos o terceiro) e retornar o valor. 
@@ -48,11 +84,39 @@ int subtrai(int a, int b, int c)
 #include <stdio.h>
 #define TAM 3
 
+void ler3Numeros(int array[])
+{
+    int i, valor;
+
+    for(i = 0; i < TAM; i++){
+        printf("Digite o número:");
+        scanf("%d", &valor);
+        array[i] = valor;
+    }
+}
+
+int main()
+{
+    int resultado[TAM];
+    
+    ler3Numeros(resultado);
+    
+    for(int i = 0; i < TAM; i++){
+        printf("%d\n", resultado[i]);
+    }
+}
+
+// 05. Crie um programa que tenha uma função ler4Numeros.
+// Essa função deve ler quatro números do usuário e retornar os quatro números.
+// A função main deve imprimir esses quatro números. Utilize struct para fazer o retorno.
+#include <stdio.h>
+#define TAM 4
+
 typedef struct numero{
     int numeros[TAM];
 }Numeros;
 
-Numeros ler3Numeros()
+Numeros ler4Numeros()
 {
     int i;
     Numeros numero1;
@@ -68,9 +132,9 @@ Numeros ler3Numeros()
 
 int main()
 {
-    Numeros resultado = ler3Numeros();
+    Numeros resultado = ler4Numeros();
     
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < TAM; i++){
         printf("%d\n", resultado.numeros[i]);
     }
 }
