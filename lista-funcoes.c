9369f81be6e5f58f6e7d5be7c199efa0dc548d26
Joelma Regina <84204532+joelmaregina@users.noreply.github.com>
2022-08-26 19:23:27 -0300

    Update lista-funcoes.c

--- lista-funcoes.c
+++ lista-funcoes.c
@@ -20,7 +20,7 @@ int soma(int a, int b)
     return a + b;
 }
 
-//  Crie um programa que tenha uma função subtrai e a função main.
+//  02. Crie um programa que tenha uma função subtrai e a função main.
 //  A função main deve ler três valores, enviar para a função subtrai.
 //  A função subtrai deve realizar a subtração dos três valores (primeiro menos o segundo menos o terceiro) e retornar o valor. 
 //  A Função main deve imprimir o resultado da subtração.
@@ -42,4 +42,37 @@ int subtrai(int a, int b, int c)
 {
     return a - b - c;
 }
+
+//  04. Crie um programa que tenha uma função ler 3Numeros. Essa função deve ler três números do usuário e retornar os três números. 
+//  A função main deve imprimir esses três números. Utilize vetor para fazer o retorno.
+#include <stdio.h>
+#define TAM 3
+
+typedef struct numero{
+    int numeros[TAM];
+}Numeros;
+
+Numeros ler3Numeros()
+{
+    int i;
+    Numeros numero1;
+    
+    for(i = 0; i < TAM; i++){
+        printf("Digite o número:");
+        scanf("%d", &numero1.numeros[i]);
+        
+    }
+    return numero1;
+    
+}
+
+int main()
+{
+    Numeros resultado = ler3Numeros();
+    
+    for(int i = 0; i < 3; i++){
+        printf("%d\n", resultado.numeros[i]);
+    }
+}
+
 
