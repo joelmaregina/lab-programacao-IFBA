d4602576af000cdc3f5042ca9dd64dd4a4dfadca
Joelma Regina <84204532+joelmaregina@users.noreply.github.com>
2022-08-26 20:24:59 -0300

    Update lista-funcoes.c

--- lista-funcoes.c
+++ lista-funcoes.c
@@ -20,6 +20,29 @@ int soma(int a, int b)
     return a + b;
 }
 
+//  02. Crie um programa que tenha uma função subtrai e a função main.
+//  A função main deve ler três valores, enviar para a função subtrai.
+//  A função subtrai deve realizar a subtração dos três valores (primeiro menos o segundo menos o terceiro) e retornar o valor. 
+//  A Função main deve imprimir o resultado da subtração.
+#include <stdio.h>
+int subtrai(int a, int b, int c);
+int main()
+{
+    int a, b, c;
+    int resultado;
+    printf("Digite os valores a serem trabalhados: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    resultado = subtrai(a,b,c);
+    printf("%d", resultado);
+}
+
+int subtrai(int a, int b, int c)
+{
+    return a - b - c;
+}
+
 //  03. Faça um programa que tenha uma função que recebe um número inteiro e retorna o fatorial deste número. 
 //  Esta função deve ainda verificar se é possível calcular o fatorial, se não for, ela deve de alguma forma retornar
 //  para a main que deu algum erro. A função main deve solicitar o valor do usuário e imprimir o fatorial dele, 
@@ -56,29 +79,6 @@ int main()
     } 
 }
 
-//  02. Crie um programa que tenha uma função subtrai e a função main.
-//  A função main deve ler três valores, enviar para a função subtrai.
-//  A função subtrai deve realizar a subtração dos três valores (primeiro menos o segundo menos o terceiro) e retornar o valor. 
-//  A Função main deve imprimir o resultado da subtração.
-#include <stdio.h>
-int subtrai(int a, int b, int c);
-int main()
-{
-    int a, b, c;
-    int resultado;
-    printf("Digite os valores a serem trabalhados: ");
-    scanf("%d", &a);
-    scanf("%d", &b);
-    scanf("%d", &c);
-    resultado = subtrai(a,b,c);
-    printf("%d", resultado);
-}
-
-int subtrai(int a, int b, int c)
-{
-    return a - b - c;
-}
-
 //  04. Crie um programa que tenha uma função ler 3Numeros. Essa função deve ler três números do usuário e retornar os três números. 
 //  A função main deve imprimir esses três números. Utilize vetor para fazer o retorno.
 #include <stdio.h>
@@ -105,6 +105,33 @@ int main()
         printf("%d\n", resultado[i]);
     }
 }
+
+// //  04. Crie um programa que tenha uma função ler 3Numeros. Essa função deve ler três números do usuário e retornar os três números. 
+// //  A função main deve imprimir esses três números. Utilize vetor para fazer o retorno.
+// #include <stdio.h>
+// #define TAM 3
+
+// void ler3Numeros(int array[TAM])
+// {
+//     int i;
+//     //array[TAM];
+    
+//     for(i = 0; i < TAM; i++){
+//         printf("Digite o número:");
+//         scanf("%d", &array[i]);
+//     }
+// }
+
+// int main()
+// {
+//     int resultado[TAM];
+//     ler3Numeros(resultado[TAM]);
+    
+//     for(int i = 0; i < TAM; i++){
+//         printf("%d\n", resultado[i]);
+//     }
+// }
+
 
 // 05. Crie um programa que tenha uma função ler4Numeros.
 // Essa função deve ler quatro números do usuário e retornar os quatro números.
