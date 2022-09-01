01496bfa7e54cc3f4e7793ee7b6833c37dce06b5
Joelma Regina <84204532+joelmaregina@users.noreply.github.com>
2022-09-01 11:41:11 -0300

    question6

--- lista-funcoes.c
+++ lista-funcoes.c
@@ -166,4 +166,31 @@ int main()
     }
 }
 
+// 06. Crie um programa que tenha uma função ler3Letras. Essa função deve ler três letras do usuário e retornar as três letras. 
+// A função main deve imprimir essas três letras.
+
+char ler3Letras(char letras[]);
+
+void main()
+{
+    char letras[4];
+    
+    ler3Letras(letras);
+    
+    printf("O 1º caracter é: %c\nO 2º caracter é: %c\nO 3º caracter é: %c", letras[0], letras[1], letras[2]);
+}
+
+char ler3Letras(char letras[])
+{
+    printf("Digite o 1º caracter: ");
+    scanf("%c", &letras[0]);
+    getchar();
+    printf("Digite o 2º caracter: ");
+    scanf("%c", &letras[1]);
+    getchar();
+    printf("Digite o 3º caracter: ");
+    scanf("%c", &letras[2]);
+}
+
+
 
